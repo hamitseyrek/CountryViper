1ab397225c784cb83c8edfcefb3f80b8696b0088
hamitseyrek <hamitseyrek@gmail.com>
2022-02-13 23:59:10 +0300

    configure Entity and Router

--- CountryViper/Router.swift
+++ CountryViper/Router.swift
@@ -7,3 +7,15 @@
 
 import Foundation
 
+protocol CountryRouter {
+    static func startExecution() -> CountryRouter
+}
+
+class CountryClassRouter : CountryRouter {
+    static func startExecution() -> CountryRouter {
+        let router = CountryClassRouter()
+        return router
+    }
+    
+    
+}
